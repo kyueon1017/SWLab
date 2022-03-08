8c4e7f266b4e34c1020ecf5ab4470cdf4a172d12
SIMONCAFFEINE <sedi1017@khu.ac.kr>
2022-03-08 15:03:30 +0900

    [Assign1_1.cpp]

---
+++ Assign1_1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//sprint1 = 5의 배수 구구단 설정하기.
+
+/*
+int main() {
+	ofstream file("myfile1.txt"); //5의 구구단 생성
+
+	int dan = 5;
+
+	if (file.is_open()) {
+		for (int i = 1; i <= 9; i++) { // file과 cout차이!! 
+			file << dan << " * " << i << " = " << dan * i << endl;
+			cout<< dan << " * " << i << " = " << dan * i << endl;
+		}
+
+		file.close();
+	}
+	else {
+		cout << "error! 파일을 열 수 없습니다\n";
+		return -1;
+	}
+
+	return 0;
+}
+*/
+
