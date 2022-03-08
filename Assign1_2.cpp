148a3ce116bb953187b9365227aa2a95f1389875
SIMONCAFFEINE <sedi1017@khu.ac.kr>
2022-03-08 15:04:25 +0900

    [Assign1_2.cpp]

---
+++ Assign1_2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
+/*
+int main() {
+	ofstream file("myfile2.txt"); 
+
+	if (file.is_open()) {
+		for (int n = 0; n <= 10; n++) {
+			file << n << " "<< pow(2, n) << endl; // txt파일에 출력
+			cout << n << " "<< pow(2, n) << endl; // vs studio화면에 출력 
+		}
+		file.close();
+	}
+	
+	else {
+		cout << "error! 파일을 열 수 없습니다\n";
+		return -1;
+	}
+
+	return 0; 
+}
+
+*/
+
